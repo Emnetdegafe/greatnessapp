a3a62afeaabc2b82a889691c4566e68872613f5e
emnetdegafe <emnetdegafe@gmail.com>
2020-02-28 09:39:20 +0100

    greatnessapp online

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Greatness awaits in the other end
         </a>
       </header>
     </div>
